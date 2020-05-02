8fd8ddb540ec3fe95b347d5c1c108cfaf3346f6f
colinkron <59934755+colinkron@users.noreply.github.com>
2020-05-02 10:29:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const jsonParser = bodyParser.json();
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -43,7 +43,7 @@ app.post('/game', (req, res) => {
 			}
 
 			if (user) {
-				res.redirect('http://localhost:5000/index.html?status=failure');
+				res.redirect('http://localhost:3000/index.html?status=failure');
 				// res.send("This username is alre");
 
 			} else {
@@ -56,7 +56,7 @@ app.post('/game', (req, res) => {
 						res.send(false);
 					} else {
 							console.log("Success: Sign up complete.");
-							res.redirect('http://localhost:5000/game.html?username=' + username + '&image=' + image);
+							res.redirect('http://localhost:3000/game.html?username=' + username + '&image=' + image);
 						}
 					});
 				}
